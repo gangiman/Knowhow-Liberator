9606bbb959fe68b6c4281150b5b90af2f72c677f
Alexandr Notchenko <notchenko@brask.ai>
2023-11-05 11:28:07 +0000

    final commit

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # Knowhow-Liberator
 
-
 Open web is dying (more like being killed), useful information communities online is being hidden in the silos. As a long time contrbutor to open online communities I want to make them sustainable again. To do that I want to automatically convert useful information, usually Question and Answer exchanges from closed platforms like Discord and publish it on open web (with original posters approval of course.). 
-To make this happen we need some way of doing complicated NLP work: finding conversation threads in unstructured chats, extracting metadata of the exchanges (who started the thread, what is it about, was it answered succcessfully), converting those exchanges
+To make this happen we need some way of doing complicated NLP work: finding conversation threads in unstructured chats, extracting metadata of the exchanges (who started the thread, what is it about, was it answered succcessfully), converting those exchanges into a static webpage. Claude 2 is great for that with it's huge context window of 100k tokens. After generating content we can serve them using Hugo.
 
 A pipeline works as follows:
 
@@ -18,11 +17,51 @@ A pipeline works as follows:
 For the testing purposes I chose "hall/help" channel in [LAION](https://laion.ai/) discord server.
 
 
-
+Using `discordchatexporter` to download json with messages.
 
 ```commandline
-docker run --rm  -v /tmp/dce_output:/dce_output tyrrrz/discordchatexporter export -t '$DTOKEN' -c '1100516714791313479' -o '/dce_output/' -f 'Json'
+docker run --rm  -v /tmp/dce_output:/dce_output tyrrrz/discordchatexporter export -t '$DTOKEN' -c '$CHANNELID' -o '/dce_output/' -f 'Json'
 ```
+
+It has too many irrelevant fields:
+
+```json
+"messages": [
+    {
+      "id": "1100516799390429195",
+      "type": "Default",
+      "timestamp": "2023-04-25T20:20:51.588+00:00",
+      "timestampEdited": null,
+      "callEndedTimestamp": null,
+      "isPinned": false,
+      "content": "cause Im a child - first",
+      "author": {
+        "id": "361704756575600660",
+        "name": "dudewhatzup",
+        "discriminator": "0000",
+        "nickname": "Dudewhatzup",
+        "color": "#749074",
+        "isBot": false,
+        "roles": [
+          {
+            "id": "1053371565934395463",
+            "name": "model-trainer",
+            "color": "#749074",
+            "position": 20
+          }
+        ],
+        "avatarUrl": "https://cdn.discordapp.com/avatars/361704756575600660/7bbc6b3a6d83e186663a6b10e46e1c20.png?size=512"
+      },
+      "attachments": [],
+      "embeds": [],
+      "stickers": [],
+      "reactions": [],
+      "mentions": []
+    },
+]
+```
+
+remove them with `jq`.
 
 ```commandline
 jq '[                  
@@ -36,6 +75,7 @@ jq '[
 ]' LAION_help_chat_1100516714791313479.json > help_messages.json
 ```
 
+then run `anthropic_hackathon.py` notebook, which converts `help_messages.json` to markdown files. 
 
 
 Example of generating papges from json: 
--- anthropic_hackathon.py
+++ anthropic_hackathon.py
@@ -1,19 +1,20 @@
 # %%
-
-from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
-
 import json
-
 with open('LAION_help_chat_1100516714791313479.json') as fh:
     data = json.loads(fh.read())
 
-data.keys()
+print(data.keys())
 
 print(data['messages'][0]['timestamp'], data['messages'][-1]['timestamp'])
 
 # %%
+from dotenv import load_dotenv
+load_dotenv()
+import os
 
-anthropic = Anthropic(api_key='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')
+
+from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
+anthropic = Anthropic(api_key=os.environ['API_KEY'])
 completion = anthropic.completions.create(
     model="claude-2",
     max_tokens_to_sample=300,
@@ -45,13 +46,12 @@ ax.set_title('Message Timeline')
 fig.autofmt_xdate()
 
 plt.show()
-# -
-
+# %%
 import networkx as nx
 
-# +
 with open('help_messages.json') as fh:
-    messages = json.loads(fh.read())
+    messages_text = fh.read()
+    messages = json.loads(messages_text)
 
 G = nx.DiGraph()
 
@@ -59,8 +59,6 @@ for message in messages:
     G.add_node(message['id'])
     if message['reply_id']:
         G.add_edge(message['id'], message['reply_id'])
-        
-# -
 
 def give_cc(graph):
     for c in nx.connected_components(graph.to_undirected()):
@@ -74,52 +72,159 @@ ccg = list(give_cc(G))
 
 graphs = sorted(ccg, key=len, reverse=True)
 
-graphs[0].nodes
+print(f'longest explicit thread has {len(graphs[0].nodes)} messages')
 # %%
-
 # got it from here https://twitter.com/hwchase17/status/1685667799174393857/
 # https://python.langchain.com/docs/integrations/chat/anthropic_functions
 
 from langchain_experimental.llms.anthropic_functions import AnthropicFunctions
 from langchain.chains import create_extraction_chain
 
+# %%
+model = AnthropicFunctions(model="claude-2", anthropic_api_key=os.environ['API_KEY'])
 
-model = AnthropicFunctions(model="claude-2")
+# %%
+# prompts from Claude 2 webapp 
 
-# example of openai function
-functions = [
-    {
-        "name": "return_recipe",
-        "description": "Return the recipe asked",
+prompt1 = "Please read all the messages in this json file and provide me with markdown text quoting last three conversations in that file."
+
+prompt2 = "Please read the file, message by message and find all discussion topics. After that list them in order of frequency of occurrence. List topics as they are mentioned exactly, no paraphrasing."
+
+prompt3 = "Read this file again, find messages regarding {TOPIC} and render them as conversations in markdown format."
+
+prompt4 = "Read this file again, find messages regarding {TOPIC} and list the id's of the relevant messages in sequential order."
+
+# %%
+metadata_extraction_function_schema = {
+        "name": "return_topics",
+        "description": "Return all discussion topics in message threads",
         "parameters": {
             "type": "object",
             "properties": {
-                "ingredients": {
+                "topic": {
                     "type": "string",
-                    "description": "The ingredients list."
+                    "description": "Topic being discussed in a thread"
                 },
-                "steps": {
+                "num_occurances": {
                     "type": "string",
-                    "description": "The recipe steps."
+                    "description": "Number of times given topic been mentioned"
+                },
+                "thread_start_id": {
+                    "type": "int64",
+                    "description": "id of message in which conversation have started"
+                },
+                "message_ids": {
+                    "type": "list",
+                    "description": "list of int64, of all messages belonging to a thread on a given topic"
                 },
             },
             },
-            "required": ["ingredients","steps"],
+            "required": ["topic","num_occurances","thread_start_id","message_ids"],
         }
-]
 
-schema = {
-    "properties": {
-        "name": {"type": "string"},
-        "height": {"type": "integer"},
-        "hair_color": {"type": "string"},
-    },
-    "required": ["name", "height"],
-}
-inp = """
-Alex is 5 feet tall. Claudia is 1 feet taller Alex and jumps higher than him. Claudia is a brunette and Alex is blonde.
-        """
+# %%
 
-chain = create_extraction_chain(schema, model)
+# %%
+# schema = {
+#     "properties": {
+#         "name": {"type": "string"},
+#         "height": {"type": "integer"},
+#         "hair_color": {"type": "string"},
+#     },
+#     "required": ["name", "height"],
+# }
 
-chain.run(inp)
+# inp = """
+# Alex is 5 feet tall. Claudia is 1 feet taller Alex and jumps higher than him. Claudia is a brunette and Alex is blonde.
+#         """
+
+# %%
+
+# %%
+metadata_extraction_function_schema = { 
+            "properties": {
+                "topic": {
+                    "type": "string",
+                    # "description": "Topic being discussed in a thread"
+                },
+                "num_occurances": {
+                    "type": "string",
+                    # "description": "Number of times given topic been mentioned"
+                },
+                "thread_start_id": {
+                    "type": "int64",
+                    # "description": "id of message in which conversation have started"
+                },
+                "message_ids": {
+                    "type": "list",
+                    # "description": "list of int64, of all messages belonging to a thread on a given topic"
+                },
+            },
+            
+            "required": ["topic", "num_occurances"]}
+
+chain = create_extraction_chain(metadata_extraction_function_schema, model)
+results = chain.run(inp)
+
+# %%
+inp = f"""{HUMAN_PROMPT}Here are all the messages:
+```
+{messages_text}
+```
+
+Please read the file, find all discussion topics. After that list them in order of frequency of occurrence. List topics as they are mentioned exactly, no paraphrasing.
+{AI_PROMPT}
+"""
+
+# %%
+
+# %%
+completion = anthropic.completions.create(
+    model="claude-2",
+    max_tokens_to_sample=4000,
+    prompt=inp,
+)
+
+# %%
+print(completion.completion)
+
+# %%
+topic_list = completion.completion.split('\n\n')[-1]
+
+# %%
+topic_list = [topic.split(' - ') for topic in topic_list.split('\n')]
+
+# %%
+from tqdm import tqdm
+
+# %%
+for _freq, _topic in tqdm(topic_list):
+    
+    _prompt = f"""{HUMAN_PROMPT}Read file with messages, find messages regarding {_topic}. 
+
+Here is the file with messages
+```
+{messages_text}
+```
+When you find the messages: rewrite them as a conversations in markdown article format for high quality blog.
+
+Return only markdown code, text should start with this block: 
+```
+---
+title: {{blogpost title}}
+date: {{timestamp of the first message in ISO format}}
+draft: false
+---
+```
+
+{AI_PROMPT}"""
+    
+    completion = anthropic.completions.create(
+    model="claude-2",
+    max_tokens_to_sample=4000,
+    prompt=_prompt)
+    slug = _topic.replace(' ', '-').replace('/', '-')
+    with open(f'site/content/post/{slug}.md', 'w+') as fh:
+        fh.write(completion.completion)
+
+# %%
